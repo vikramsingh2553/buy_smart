3e712d94dcc33aacf0b0a5cbe44c0963e2e9b44f
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-17 19:11:41 +0530

    create product provider and service

---
+++ lib/product/service/product_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:buy_smart/product/model/product_model.dart';
+import 'package:buy_smart/product/shared/api_endpoints.dart';
+import 'package:http/http.dart' as http;
+
+class ProductService {
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await http.get(
+      Uri.parse(ApiEndpoints.product),
+      headers: {
+        'Authorization': 'Bearer ${ApiEndpoints.authToken}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<ProductModel> products = (data['products'] as List)
+          .map((item) => ProductModel.fromJson(item))
+          .toList();
+      return products;
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+}
