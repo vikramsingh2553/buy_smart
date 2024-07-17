4a4ac9a5538278ef181a6538b3dd99004fd3ea02
Vikram <vikramsheoran2277@gmail.com>
2024-07-17 21:02:29 +0530

    auth ui design

---
+++ lib/auth/provider/user_provider.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+import '../model/user_model.dart';
+import '../service/user_api_service.dart';
+
+class UserProvider extends ChangeNotifier {
+  final UserApiService _userService = UserApiService();
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  Future<bool> register(UserModel user) async {
+    _setLoading(true);
+    final success = await _userService.register(user);
+    _setLoading(false);
+    return success;
+  }
+
+  Future<bool> login(UserModel user) async {
+    _setLoading(true);
+    final success = await _userService.login(user);
+    _setLoading(false);
+    return success;
+  }
+
+  void _setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
-import 'package:buy_smart/auth/ui/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'auth/provider/user_provider.dart';
+import 'auth/ui/welcome_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Buy Smart',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.cyan,
       ),
-      home: WelcomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
-
