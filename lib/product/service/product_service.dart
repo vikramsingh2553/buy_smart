3fb36c5c5fc730590128c6c48d8e647706b2b171
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-20 16:43:38 +0530

    update product service and provider for do functionality favorite icon

--- lib/product/service/product_service.dart
+++ lib/product/service/product_service.dart
@@ -20,4 +20,43 @@ class ProductService {
       throw Exception('Failed to load products');
     }
   }
+
+  Future<List<ProductModel>> fetchFavorites() async {
+    final response = await http.get(
+      Uri.parse(ApiEndpoints.product),
+      headers: {
+        'Authorization': 'Bearer ${ApiEndpoints.authToken}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      List<ProductModel> favorites = data.map((item) => ProductModel.fromJson(item)).toList();
+      return favorites;
+    } else {
+      throw Exception('Failed to load favorites');
+    }
+  }
+
+  Future<bool> addToFavorites(String productId) async {
+    final response = await http.post(
+      Uri.parse('${ApiEndpoints.product}/$productId'),
+      headers: {
+        'Authorization': 'Bearer ${ApiEndpoints.authToken}',
+      },
+    );
+
+    return response.statusCode == 200;
+  }
+
+  Future<bool> removeFromFavorites(String productId) async {
+    final response = await http.delete(
+      Uri.parse('${ApiEndpoints.product}/$productId'),
+      headers: {
+        'Authorization': 'Bearer ${ApiEndpoints.authToken}',
+      },
+    );
+
+    return response.statusCode == 200;
+  }
 }
